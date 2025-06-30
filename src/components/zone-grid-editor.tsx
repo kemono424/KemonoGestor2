15a632a36b7c96d1aab899ca043d7166d9c57599
sebastian Medina <lsmcrack424@gmail.com>
2025-06-30 09:16:54 +0000

    haz las lineas de la cuadricula color rojo

--- src/components/zone-grid-editor.tsx
+++ src/components/zone-grid-editor.tsx
@@ -13,7 +13,6 @@ export default function ZoneGridEditor({
   gridData,
   onCellClick,
 }: ZoneGridEditorProps) {
-
   const handleMapClick = (e: MapLayerMouseEvent) => {
     const features = e.target.queryRenderedFeatures(e.point, {
       layers: ['grid-fill'],
@@ -30,33 +29,33 @@ export default function ZoneGridEditor({
   return (
     <div className="h-[calc(100vh-14rem)] min-h-[500px] w-full rounded-lg overflow-hidden border">
       <Map
+        mapboxAccessToken={process.env.NEXT_PUBLIC_MAPBOX_TOKEN}
         initialViewState={{
           longitude: -65.4117,
           latitude: -24.7859,
           zoom: 12,
         }}
-        mapboxAccessToken={process.env.NEXT_PUBLIC_MAPBOX_TOKEN}
         style={{ width: '100%', height: '100%' }}
         mapStyle="mapbox://styles/mapbox/dark-v11"
         onClick={handleMapClick}
         interactiveLayerIds={['grid-fill']}
       >
         <Source id="grid-source" type="geojson" data={gridData}>
-           <Layer
+          <Layer
             id="grid-fill"
             type="fill"
             paint={{
               'fill-color': ['get', 'color'],
-              'fill-opacity': 0.2,
+              'fill-opacity': 0.15,
             }}
           />
           <Layer
             id="grid-stroke"
             type="line"
             paint={{
-              'line-color': '#FFFFFF',
+              'line-color': '#FF0000',
               'line-width': 1,
-              'line-opacity': 0.3,
+              'line-opacity': 0.8,
             }}
           />
         </Source>
